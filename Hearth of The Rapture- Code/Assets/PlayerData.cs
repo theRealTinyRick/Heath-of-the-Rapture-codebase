8e333af13e594a9d8ea43f4d8c8598b084bc9b66
theRealTinyRick <31013885+theRealTinyRick@users.noreply.github.com>
2018-06-11 13:58:33 -0400

    Player Data Started
    Add a simple interface for interacting with PlayerStats. Also wrote functions to
    persistantly save and load PlayerStats with a class called PlayerData

---
+++ Hearth of The Rapture- Code/Assets/PlayerData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+[Serializable]
+public class PlayerData{
+
+	public float maxHealth = 0;
+	public float currentHealth = 0;
+	public float stamina = 0;
+	public float fatigue = 0;
+	public float mentalHealth = 0;
+	public float hunger = 0;
+	public float thirst = 0;
+
+	//public string[] inventory; add this to the constructor later in case we want to store that player inventory as an array of strings in the same external file. 
+
+	public PlayerData(float maxHealth, float currentHealth, float stamina, float fatigue, float mentalHealth, float hunger, float thirst){
+		this.maxHealth = maxHealth;
+		this.currentHealth = currentHealth; 
+		this.stamina = stamina;
+		this.fatigue = fatigue;
+		this.mentalHealth = mentalHealth;
+		this.mentalHealth = mentalHealth;
+		this.hunger = hunger;
+		this.thirst = thirst;
+	}
+}
---
+++ Hearth of The Rapture- Code/Assets/PlayerStats.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour {
+
+    private string playerDataPath; //The path where we store Player Data. 
+
+    //I set the below variable as properties so that they are readonly out side of this class. If you want to alter a value please do so with one of the below methods.
+    //We dont want these values altered all over the game.
+    private float maxHealth = 0;
+    public float MaxHealth{
+        get{return maxHealth;}
+    }
+
+	private float currentHealth = 0;
+    public float CurrentHealth{
+        get{return currentHealth;}
+    }
+
+	private float stamina = 0;
+    public float Stamina{
+        get{return stamina;}
+    }
+
+	private float fatigue = 0;
+    public float Fatigue{
+        get{return fatigue;}
+    }
+
+	private float mentalHealth = 0;
+    public float MentalHealth{
+        get{return mentalHealth;}
+    }
+
+	private float hunger = 0;
+    public float Hunger{
+        get {return hunger;}
+    }
+	private float thirst = 0;
+    public float Thirst{
+        get{return thirst;}
+    }
+
+	//public string[] inventory;we can use this to dynamically create the player inventory at when the game loads. If we go that direction.
+    [SerializeField] Slider healthSlider;
+    [SerializeField] Slider staminaSlider;
+
+    private void Start(){
+        playerDataPath = Application.persistentDataPath + "/playerData.dat";
+
+        Debug.Log("Remember to delete the save file for playerdata from your device as not to corrupt data during the dev process. You can find the file at: " + 
+        playerDataPath + " love, Gadnuuk");
+
+    }
+
+    public void DealDamage(float dmg){
+        currentHealth -= dmg;
+        if(currentHealth < 0){
+            currentHealth = 0;
+        }
+    }
+
+    public void HealDamage(float heamAmt){
+        currentHealth += heamAmt;
+        if(currentHealth > maxHealth){
+            currentHealth = maxHealth;
+        }
+    }
+
+    public void SetPlayerStatUI(){
+
+    }
+
+	public void SavePlayerData(){
+        //This will save the players stats from this script to an external file found at the persistant data path. Th
+        //The file created be almost unreadable to most people but nit entirely. 
+        //We can change the location of the Save and Load functions later. 
+        BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Create(playerDataPath);
+        PlayerData data = new PlayerData(maxHealth, currentHealth, stamina, fatigue, mentalHealth, hunger, thirst);
+
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    public void LoadPlayerData(){
+        //get the file where we stored the player data, open it, then cast it as a PlayeData object. Then use the data to load the Player Stats in this script
+        if(File.Exists(playerDataPath)){
+            BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open(playerDataPath, FileMode.Open);
+            PlayerData data = (PlayerData)bf.Deserialize(file);
+            file.Close();
+
+            maxHealth = data.maxHealth;
+            currentHealth = data.currentHealth;
+            stamina = data.stamina;
+            fatigue = data.fatigue;
+            mentalHealth = data.mentalHealth;
+            hunger = data.hunger;
+            thirst = data.thirst;
+        }
+    }
+}
