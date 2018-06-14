f08e77e84501f89cd1a2cf3efeba9c5ef4ad3a67
theRealTinyRick <31013885+theRealTinyRick@users.noreply.github.com>
2018-06-14 09:54:39 -0400

    more fleshing out

--- Hearth of The Rapture- Code/Assets/Scripts/PlayerStats.cs
+++ Hearth of The Rapture- Code/Assets/Scripts/PlayerStats.cs
@@ -17,6 +17,12 @@ public class PlayerStats : MonoBehaviour {
     public float CurrentHealth{
         get{return currentHealth;}
     }
+
+    private float maxStamina = 100;
+    public float MaxStamina{
+        get{return maxStamina;}
+    }
+
 	private float stamina = 100;
     public float Stamina{
         get{return stamina;}
@@ -37,6 +43,7 @@ public class PlayerStats : MonoBehaviour {
     public float Hunger{
         get {return hunger;}
     }
+
 	private float thirst = 100;
     public float Thirst{
         get{return thirst;}
@@ -50,22 +57,25 @@ public class PlayerStats : MonoBehaviour {
     [SerializeField] private Image thirstSlider;
     [SerializeField] private Image mentalHealthSlider;
 
+    [SerializeField] GameObject[] statusMessages;
+
     private bool playerMayUseStamina = true;
     private float stasisTime = 1;
 
     private void Start(){
-
         currentHealth = maxHealth;
+        stamina = maxStamina;
     }
 
     private void Update(){
         SetPlayerStatUI();
         DecreasePlayerState();
+        DisplayStatusMessages();
 
 
         //Use the below code to test the stamina system. 
         if(Input.GetMouseButton(0) && playerMayUseStamina){
-            UseStamina();
+            UseStaminaOverTime();
         }
 
         if(!Input.GetMouseButton(0) || !playerMayUseStamina){
@@ -92,10 +102,23 @@ public class PlayerStats : MonoBehaviour {
         currentHealth = Mathf.MoveTowards(currentHealth, maxHealth, healOverTimeScale * Time.deltaTime);
     }
 
-    public void UseStamina(){
+    public void UseStaminaOverTime(){
         float rateOfStaminaLoss = 20;
         stamina = Mathf.MoveTowards(stamina, 0, rateOfStaminaLoss * Time.deltaTime);
-        
+        TriggerStasis();
+    }
+
+    public void UseStamina(float useAmt){
+        stamina -= useAmt;
+        TriggerStasis();
+    }
+
+    private void IncreaseStamina(){
+        float rateOfStaminaGain = 30;
+        stamina = Mathf.MoveTowards(stamina, maxStamina, rateOfStaminaGain * Time.deltaTime);
+    }
+
+    private void TriggerStasis(){
         if(stamina <= 0){
             fatigue -= 10.0f;
             if(fatigue < 0){
@@ -110,11 +133,6 @@ public class PlayerStats : MonoBehaviour {
         }
     }
 
-    private void IncreaseStamina(){
-        float rateOfStaminaGain = 30;
-        stamina = Mathf.MoveTowards(stamina, 100, rateOfStaminaGain * Time.deltaTime);
-    }
-
     private IEnumerator Stasis(){
         //this function will be called to tell the player that he has become to tired and must rest for a few seconds before using stamina again. *See UseStamina
         playerMayUseStamina = false;
@@ -124,7 +142,7 @@ public class PlayerStats : MonoBehaviour {
         yield return null;
     }
     
-    public void DecreasePlayerState(){
+    private void DecreasePlayerState(){
         float stateDecreaseSpeed = 1;
         fatigue = Mathf.MoveTowards(fatigue, 0, stateDecreaseSpeed * Time.deltaTime);
         hunger = Mathf.MoveTowards(hunger, 0, stateDecreaseSpeed * Time.deltaTime);
@@ -132,17 +150,50 @@ public class PlayerStats : MonoBehaviour {
         mentalHealth = Mathf.MoveTowards(mentalHealth, 0, stateDecreaseSpeed * Time.deltaTime);
     }
 
-    public void SetPlayerStatUI(){
+    private void SetPlayerStatUI(){
         healthSlider.maxValue = maxHealth;
         healthSlider.value = Mathf.Lerp(healthSlider.value, currentHealth, .3f);
 
-        staminaSlider.maxValue = 100;
+        staminaSlider.maxValue = maxStamina;
         staminaSlider.value = Mathf.Lerp(staminaSlider.value, stamina, .3f);
 
+        //I used fillAmount instead of Value below because these circular sliders are actually just images. 
         fatigueSlider.fillAmount = fatigue * .01f;
         thirstSlider.fillAmount = thirst * .01f;
         hungerSlider.fillAmount = hunger * .01f;
         mentalHealthSlider.fillAmount = mentalHealth * .01f;
+    }
+
+    private  void DisplayStatusMessages(){
+        if(currentHealth <= (maxHealth/5)){
+            statusMessages[0].SetActive(true);
+        }else{
+            statusMessages[0].SetActive(false);
+        }
+
+        if(fatigue <= (100/5)){
+            statusMessages[1].SetActive(true);
+        }else{
+            statusMessages[1].SetActive(false);
+        }
+
+        if(hunger <= (100/5)){
+            statusMessages[2].SetActive(true);
+        }else{
+            statusMessages[2].SetActive(false);
+        }
+
+        if(thirst <= (100/5)){
+            statusMessages[3].SetActive(true);
+        }else{
+            statusMessages[3].SetActive(false);
+        }
+
+        if(mentalHealth <= (100/5)){
+            statusMessages[4].SetActive(true);
+        }else{
+            statusMessages[4].SetActive(false);
+        }
     }
 
     public void LoadStats(PlayerData data){
