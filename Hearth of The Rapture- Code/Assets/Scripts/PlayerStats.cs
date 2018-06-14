3172a601886ced3ba5fa432dad2b58efaa6e35b7
theRealTinyRick <31013885+theRealTinyRick@users.noreply.github.com>
2018-06-14 09:29:06 -0400

    Finished Prototyping the stamina system

--- Hearth of The Rapture- Code/Assets/Scripts/PlayerStats.cs
+++ Hearth of The Rapture- Code/Assets/Scripts/PlayerStats.cs
@@ -6,9 +6,6 @@ using System.IO;
 using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour {
-
-   
-
     //I set the below variable as properties so that they are readonly out side of this class. If you want to alter a value please do so with one of the below methods.
     //We dont want these values altered all over the game.
     private float maxHealth = 100;
@@ -20,13 +17,7 @@ public class PlayerStats : MonoBehaviour {
     public float CurrentHealth{
         get{return currentHealth;}
     }
-
-    private float maxStamina = 100;
-    public float MaxStamina{
-        get{return maxStamina;}
-    }
-
-	private float stamina = 0;
+	private float stamina = 100;
     public float Stamina{
         get{return stamina;}
     }
@@ -52,22 +43,34 @@ public class PlayerStats : MonoBehaviour {
     }
 
     //public string[] inventory;we can use this to dynamically create the player inventory at when the game loads. If we go that direction.
-    [SerializeField] Slider healthSlider;
-    [SerializeField] Slider staminaSlider;
-    [SerializeField] Image fatigueSlider;
-    [SerializeField] Image hungerSlider;
-    [SerializeField] Image thirstSlider;
-    [SerializeField] Image mentalHealthSlider;
+    [SerializeField] private Slider healthSlider;
+    [SerializeField] private Slider staminaSlider;
+    [SerializeField] private Image fatigueSlider;
+    [SerializeField] private Image hungerSlider;
+    [SerializeField] private Image thirstSlider;
+    [SerializeField] private Image mentalHealthSlider;
+
+    private bool playerMayUseStamina = true;
+    private float stasisTime = 1;
 
     private void Start(){
 
         currentHealth = maxHealth;
-        stamina = maxStamina;
     }
 
     private void Update(){
         SetPlayerStatUI();
         DecreasePlayerState();
+
+
+        //Use the below code to test the stamina system. 
+        if(Input.GetMouseButton(0) && playerMayUseStamina){
+            UseStamina();
+        }
+
+        if(!Input.GetMouseButton(0) || !playerMayUseStamina){
+            IncreaseStamina();
+        }
     }
 
     public void DealDamage(float dmg){
@@ -89,22 +92,57 @@ public class PlayerStats : MonoBehaviour {
         currentHealth = Mathf.MoveTowards(currentHealth, maxHealth, healOverTimeScale * Time.deltaTime);
     }
 
+    public void UseStamina(){
+        float rateOfStaminaLoss = 20;
+        stamina = Mathf.MoveTowards(stamina, 0, rateOfStaminaLoss * Time.deltaTime);
+        
+        if(stamina <= 0){
+            fatigue -= 10.0f;
+            if(fatigue < 0){
+                fatigue = 0;
+            }
 
-    public void SetPlayerStatUI(){
-        healthSlider.maxValue = maxHealth;
-        healthSlider.value = Mathf.Lerp(healthSlider.value, currentHealth, .3f);
-
-        staminaSlider.maxValue = maxStamina;
-        staminaSlider.value = Mathf.Lerp(staminaSlider.value, stamina, .3f);
-
+            thirst -= 25.0f;
+            if(thirst < 0){
+                thirst = 0; 
+            }
+            StartCoroutine(Stasis());
+        }
     }
 
+    private void IncreaseStamina(){
+        float rateOfStaminaGain = 30;
+        stamina = Mathf.MoveTowards(stamina, 100, rateOfStaminaGain * Time.deltaTime);
+    }
+
+    private IEnumerator Stasis(){
+        //this function will be called to tell the player that he has become to tired and must rest for a few seconds before using stamina again. *See UseStamina
+        playerMayUseStamina = false;
+        yield return new WaitForSeconds(stasisTime);
+        float time = Time.time;
+        playerMayUseStamina = true;
+        yield return null;
+    }
+    
     public void DecreasePlayerState(){
         float stateDecreaseSpeed = 1;
         fatigue = Mathf.MoveTowards(fatigue, 0, stateDecreaseSpeed * Time.deltaTime);
         hunger = Mathf.MoveTowards(hunger, 0, stateDecreaseSpeed * Time.deltaTime);
         thirst = Mathf.MoveTowards(thirst, 0, stateDecreaseSpeed * Time.deltaTime);
         mentalHealth = Mathf.MoveTowards(mentalHealth, 0, stateDecreaseSpeed * Time.deltaTime);
+    }
+
+    public void SetPlayerStatUI(){
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = Mathf.Lerp(healthSlider.value, currentHealth, .3f);
+
+        staminaSlider.maxValue = 100;
+        staminaSlider.value = Mathf.Lerp(staminaSlider.value, stamina, .3f);
+
+        fatigueSlider.fillAmount = fatigue * .01f;
+        thirstSlider.fillAmount = thirst * .01f;
+        hungerSlider.fillAmount = hunger * .01f;
+        mentalHealthSlider.fillAmount = mentalHealth * .01f;
     }
 
     public void LoadStats(PlayerData data){
